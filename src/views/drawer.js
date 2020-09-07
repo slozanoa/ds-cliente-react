4ca2e48d20a45d398398ee4e5ff2c5811743cd5d
santiago lozano <santiagolozal@outlook.com>
2020-09-06 19:25:43 -0500

    hola2

--- src/views/drawer.js
+++ src/views/drawer.js
@@ -160,7 +160,7 @@ return (
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <Typography paragraph>
-        hola
+        hola2 porque si
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
           facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
